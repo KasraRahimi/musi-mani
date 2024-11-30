47e6ad1befad88bcbf193193965f404006ca0804
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2024-11-30 02:53:33 -0500

    FIX: Bot crashing because of free floating callback

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import commands
 from constants import TOKEN
-from interactions import Client, Intents, listen, SlashCommand
+from interactions import Client, Intents, listen, SlashCommand, Status
+
+
+def register_commands(bot: Client):
+    for command_name in commands.__all__:
+        command: SlashCommand = getattr(commands, command_name)
+        bot.add_interaction(command)
 
 if __name__ == "__main__":
     bot = Client(token=TOKEN, intents=Intents.DEFAULT)
 
-    for command_name in commands.__all__:
-        command: SlashCommand= getattr(commands, command_name)
-        print(f"Adding command {command.name} with resolved name {command.resolved_name}")
-        bot.add_interaction(command)
+    register_commands(bot)
 
     @listen()
     async def on_ready():
         print("Bot is ready")
-    
+
     bot.start()
     
