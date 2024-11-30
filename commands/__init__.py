a34caaac8c4918c9690b1d8cb686def6ef562252
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2024-11-30 03:25:47 -0500

    FEAT: added new anonymous message command

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,7 +1,8 @@
-from .misc import name, ping
+from .misc import name, ping, anonymous
 
 __all__ = [
     # misc commands
     'name',
     'ping',
+    'anonymous'
 ]
--- commands/misc/__init__.py
+++ commands/misc/__init__.py
@@ -1,7 +1,9 @@
 from .name import name
 from .ping import ping
+from .anonymous import anonymous
 
 __all__ = [
     "name",
     "ping",
+    "anonymous"
 ]
