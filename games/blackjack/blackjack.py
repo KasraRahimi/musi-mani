08b2c42f666538187184c671a842d52e2da7a7cf
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2024-12-01 03:10:09 -0500

    FEAT: Finished blackjack class

--- games/blackjack/blackjack.py
+++ games/blackjack/blackjack.py
@@ -17,20 +17,21 @@ class Blackjack:
         self.bet: int = bet
         self.deck: list[Card] = self.__get_shuffled_deck(num_of_decks)
         self.canDouble: bool = True
+        self.winnings: int = 0
 
         self.player_hand: list[Card] = []
         self.dealer_hand: list[Card] = []
 
     @classmethod
-    def get_deck(cls):
+    def get_deck(cls) -> list[Card]:
         return [Card(rank, suit) for suit in Suit for rank in Rank]
 
-    def __get_shuffled_deck(self, num_of_decks):
+    def __get_shuffled_deck(self, num_of_decks) -> list[Card]:
         deck = self.get_deck() * num_of_decks
         shuffle(deck)
         return deck
 
-    def __get_hand_value(self, deck: list[Card]):
+    def __get_hand_value(self, deck: list[Card]) -> int:
         ace_count = 0
         total = 0
         for card in deck:
@@ -45,12 +46,18 @@ class Blackjack:
         return total
 
     @property
-    def player_value(self):
+    def player_value(self) -> int:
         return self.__get_hand_value(self.player_hand)
 
     @property
-    def dealer_value(self):
+    def dealer_value(self) -> int:
         return self.__get_hand_value(self.dealer_hand)
+
+    @property
+    def dealer_first_card(self) -> Card | None:
+        if len(self.dealer_hand) < 1:
+            return None
+        return self.dealer_hand[0]
 
     def start(self):
         if self.outcome is not None:
@@ -72,14 +79,83 @@ class Blackjack:
             self.outcome = Outcome.BLACKJACK
 
     def hit(self):
+        if self.outcome is not None:
+            return
+
         self.canDouble = False
+        self.player_hand.append(self.deck.pop())
+
+        if self.player_value > 21:
+            self.outcome = Outcome.LOSE
 
     def stand(self):
+        if self.outcome is not None:
+            return
+
         self.canDouble = False
+
+        while self.dealer_value < 17:
+            self.dealer_hand.append(self.deck.pop())
+
+        if self.dealer_value > 21 or self.dealer_value < self.player_value:
+            self.outcome = Outcome.WIN
+            return
+        if self.dealer_value > self.player_value:
+            self.outcome = Outcome.LOSE
+            return
+        if self.dealer_value == self.player_value:
+            self.outcome = Outcome.DRAW
+            return
+
+        self.__calculate_winnings()
 
     def double(self):
+        if self.outcome is not None:
+            return
+
         self.canDouble = False
 
+    def __calculate_winnings(self):
+        match self.outcome:
+            case Outcome.WIN:
+                self.winnings = self.bet * self.WIN_MULTIPLIER
+            case Outcome.LOSE:
+                self.winnings = 0
+            case Outcome.BLACKJACK:
+                self.winnings = self.bet * self.BLACKJACK_MULTIPLIER
+            case Outcome.DRAW:
+                self.winnings = self.bet
 
-if __name__ == "__main__":
-    pass
+# def get_player_input() -> int:
+#     print("1 - hit")
+#     print("2 - stand")
+#     return int(input("> "))
+#
+# def print_cards(hand: list[Card]):
+#     for card in hand:
+#         print("• {}".format(card))
+#
+# if __name__ == "__main__":
+#     print("== Blackjack ==")
+#     blackjack = Blackjack(100)
+#     blackjack.start()
+#     while blackjack.outcome is None:
+#         print("Dealers first card: {}".format(blackjack.dealer_first_card))
+#         print("\n~ your cards ~")
+#         print_cards(blackjack.player_hand)
+#         print(f"Value: {blackjack.player_value}\n")
+#         choice = get_player_input()
+#         match choice:
+#             case 1:
+#                 blackjack.hit()
+#             case 2:
+#                 blackjack.stand()
+#             case 3:
+#                 break
+#     print("~~ Dealer's final cards ~~")
+#     print_cards(blackjack.dealer_hand)
+#     print("value: {}".format(blackjack.dealer_value))
+#     print("\n~~ Player's final cards ~~")
+#     print_cards(blackjack.player_hand)
+#     print(f"value: {blackjack.player_value}")
+#     print("Outcome: {}".format(blackjack.outcome))
