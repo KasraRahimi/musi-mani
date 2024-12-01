f011b76d99a1365a14945e90ce1c2af91c831b91
KasraRahimi <kasra.rahimi@polymtl.ca>
2024-11-30 23:37:09 -0500

    FEAT: Added useful print statements for bot loading

--- main.py
+++ main.py
@@ -5,16 +5,20 @@ from interactions import Client, Intents, listen, SlashCommand, Listener
 
 
 def register_commands(bot: Client):
+    print("== Registering commands ==")
     for command_name in commands.__all__:
         print("Loading {} command".format(command_name))
         command: SlashCommand = getattr(commands, command_name)
         bot.add_interaction(command)
+    print("~~ Loaded {} (/) commands ~~\n".format(len(commands.__all__)))
 
 def register_events(bot: Client):
+    print("== Registering events ==")
     for event_name in events.__all__:
         print("Loading {} event".format(event_name))
         event: Listener = getattr(events, event_name)
         bot.add_listener(event)
+    print("~~ Loaded {} events ~~\n".format(len(events.__all__)))
 
 if __name__ == "__main__":
     bot = Client(token=TOKEN, intents=Intents.DEFAULT)
