d83ab4d41b80bf571888d52c6029916695319861
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2024-12-01 02:25:08 -0500

    FEAT: added first blackjack move

--- games/blackjack/blackjack.py
+++ games/blackjack/blackjack.py
@@ -9,23 +9,71 @@ class Outcome(Enum):
     DRAW = 3
 
 class Blackjack:
+    WIN_MULTIPLIER = 2
+    BLACKJACK_MULTIPLIER = 2.5
+
     def __init__(self, bet: int, num_of_decks: int=2):
         self.outcome: None | Outcome = None
         self.bet: int = bet
         self.deck: list[Card] = self.__get_shuffled_deck(num_of_decks)
         self.canDouble: bool = True
 
-        self.player_hand, self.dealer_hand = [], []
+        self.player_hand: list[Card] = []
+        self.dealer_hand: list[Card] = []
 
+    @classmethod
+    def get_deck(cls):
+        return [Card(rank, suit) for suit in Suit for rank in Rank]
 
     def __get_shuffled_deck(self, num_of_decks):
         deck = self.get_deck() * num_of_decks
         shuffle(deck)
         return deck
 
-    @classmethod
-    def get_deck(cls):
-        return [Card(rank, suit) for suit in Suit for rank in Rank]
+    def __get_hand_value(self, deck: list[Card]):
+        ace_count = 0
+        total = 0
+        for card in deck:
+            total += card.value
+            if card.rank == Rank.ACE:
+                ace_count += 1
+
+        while ace_count > 0 and total + 10 <= 21:
+            total += 10
+            ace_count -= 1
+
+        return total
+
+    @property
+    def player_value(self):
+        return self.__get_hand_value(self.player_hand)
+
+    @property
+    def dealer_value(self):
+        return self.__get_hand_value(self.dealer_hand)
+
+    def start(self):
+        if self.outcome is not None:
+            return
+
+        for _ in range(2):
+            self.player_hand.append(self.deck.pop())
+        for _ in range(2):
+            self.dealer_hand.append(self.deck.pop())
+
+        if self.player_value > 21:
+            self.outcome = Outcome.LOSE
+            return
+
+        if self.player_value == 21:
+            if self.dealer_value == 21:
+                self.outcome = Outcome.DRAW
+                return
+            self.outcome = Outcome.BLACKJACK
+
+
+
+
 
 if __name__ == "__main__":
     pass
