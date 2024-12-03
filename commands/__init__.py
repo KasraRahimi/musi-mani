7c2a26003d80038dbb90fdd741d3fdf32c34e170
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2024-12-03 12:54:56 -0500

    FEAT: added balance command to commands init

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,11 +1,15 @@
 from .misc import name, ping, anonymous
 from .games import blackjack, coin_toss
+from .talan import balance
 
 __all__ = [
     # misc commands
     'name',
     'ping',
     'anonymous',
+    # games
     'blackjack',
-    'coin_toss'
+    'coin_toss',
+    # talan
+    'balance'
 ]
