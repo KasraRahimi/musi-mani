d1281220a34a016148cf01db8242bb5ab5fdcc04
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2024-12-03 13:36:46 -0500

    FEAT: command error event checks if commandCheckFailure was thrown

--- events/command_error.py
+++ events/command_error.py
@@ -2,10 +2,14 @@ import traceback
 
 from interactions import Listener
 from interactions.api.events import CommandError
+from interactions.client.errors import CommandCheckFailure
 
 EVENT_NAME = "command_error"
 
 async def callback(event: CommandError):
+    if isinstance(event.error, CommandCheckFailure):
+        await event.ctx.send("You do not have the permissions to use this command", ephemeral=True)
+        return
     print(traceback.print_exception(event.error))
     await event.ctx.send("Something went wrong", ephemeral=True)
 
