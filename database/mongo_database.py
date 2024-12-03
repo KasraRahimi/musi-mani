428e00230f2fff5e4f95dbfd9a9a848d7b8a514a
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2024-12-03 10:26:00 -0500

    FEAT: added singleton database class

---
+++ database/mongo_database.py
@@ -0,0 +1,28 @@
+from pymongo import MongoClient
+from constants import MONGO_DB_URI
+
+DATABASE_NAME = "musi-mani"
+
+class BotDatabase:
+    __instance = None
+
+    __client = None
+    __database = None
+
+    def __new__(cls):
+        if cls.__instance is None:
+            print("Creating instance of database")
+            cls.__instance = super().__new__(cls)
+            cls.__instance.__initialize()
+        return cls.__instance
+
+    def __initialize(self):
+        self.__client = MongoClient(MONGO_DB_URI)
+        self.__database = self.__client.get_database(DATABASE_NAME)
+
+    @property
+    def db(self):
+        return self.__database
+
+    def get_collection(self, collection_name: str):
+        return self.__database.get_collection(collection_name)
