83b3824a20784d365a5a93b3a162d2e62fb505b2
KasraRahimi <kasra.rahimi@polymtl.ca>
2024-12-05 06:38:03 -0500

    FEAT: added last call to init files

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,5 +1,5 @@
 from .misc import name, ping, anonymous
-from .games import blackjack, coin_toss
+from .games import blackjack, coin_toss, last_call
 from .talan import balance, add_funds, give, reward, weka_ale
 
 __all__ = [
@@ -10,6 +10,7 @@ __all__ = [
     # games
     'blackjack',
     'coin_toss',
+    'last_call',
     # talan
     'balance',
     'add_funds',
--- commands/games/__init__.py
+++ commands/games/__init__.py
@@ -1,7 +1,9 @@
 from .blackjack import blackjack
 from .coin_toss import coin_toss
+from .last_call import last_call
 
 __all__ = [
     "blackjack",
-    "coin_toss"
+    "coin_toss",
+    "last_call"
 ]
