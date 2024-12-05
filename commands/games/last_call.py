ff63bb104ec7a453340b382076b9a4f38c62c542
KasraRahimi <kasra.rahimi@polymtl.ca>
2024-12-05 06:37:12 -0500

    FIX: command name

--- commands/games/last_call.py
+++ commands/games/last_call.py
@@ -6,7 +6,7 @@ from .constants import COMMAND_NAME, COMMAND_DESCRIPTION, BET_OPTION
 @slash_command(
     name=COMMAND_NAME,
     description=COMMAND_DESCRIPTION,
-    sub_cmd_name='last_call',
+    sub_cmd_name='last-call',
     sub_cmd_description='place a bet on a game where you have to cash out before you crash',
     options=[BET_OPTION]
 )
