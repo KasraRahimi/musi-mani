888d23c73c066694e33eb9f51870c7cb3a69cc9c
KasraRahimi <kasra.rahimi@polymtl.ca>
2024-12-07 12:19:28 -0500

    FEAT: wrote boilerplate code for new slash command

---
+++ commands/games/rps.py
@@ -0,0 +1,14 @@
+from interactions import slash_command, SlashContext
+
+from .constants import COMMAND_NAME, COMMAND_DESCRIPTION, BET_OPTION
+
+
+@slash_command(
+    name=COMMAND_NAME,
+    description=COMMAND_DESCRIPTION,
+    sub_cmd_name="rps",
+    sub_cmd_description="place a bet on a game of rock paper scissors",
+    options=[BET_OPTION]
+)
+async def rps(ctx: SlashContext, bet: int):
+    pass
