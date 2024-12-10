3577b7e29e99d67de219aee65fd07702612025e5
KasraRahimi <kasra.rahimi@polymtl.ca>
2024-12-10 06:53:59 -0500

    FEAT: temporary user profile embed

--- commands/misc/profile.py
+++ commands/misc/profile.py
@@ -1,4 +1,4 @@
-from interactions import slash_command, SlashContext
+from interactions import slash_command, SlashContext, Embed, Color, EmbedField, EmbedAttachment
 
 
 @slash_command(
@@ -6,4 +6,12 @@ from interactions import slash_command, SlashContext
     description="See your user profile with the bot"
 )
 async def profile(ctx: SlashContext):
-    pass
+    image = EmbedAttachment(url=ctx.author.avatar_url)
+    embed = Embed(
+        title=ctx.author.display_name,
+        thumbnail=image,
+        color=Color((0, 255, 0)),
+        description=f"{ctx.author.display_name}'s profile",
+        fields=[EmbedField(name='hi', value="hello", inline=False)],
+    )
+    await ctx.send(embed=embed)
