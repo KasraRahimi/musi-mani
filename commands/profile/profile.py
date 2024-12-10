49c3363993c89e43575cdae8b9ae3a19bf151652
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2024-12-10 16:52:22 -0500

    FEAT: Random quote no longer in profile card

--- commands/profile/profile.py
+++ commands/profile/profile.py
@@ -1,17 +1,25 @@
 from random import randint
-from interactions import slash_command, SlashContext, Embed, Color, EmbedField, EmbedAttachment
+from interactions import slash_command, SlashContext, Embed, Color, EmbedField, EmbedAttachment, SlashCommandOption, \
+    OptionType, User, Member
 from requests import get
 from database import BotUser
 
-BASE_URL = "https://api.quotable.io"
+# BASE_URL = "https://api.quotable.io"
 
-def get_random_quote() -> str:
-    for _ in range(3):
-        response = get(f"{BASE_URL}/quotes/random", verify=False)
-        if response.status_code != 200:
-            continue
-        return response.json().pop()["content"]
-    return "No description"
+USER_OPTION = SlashCommandOption(
+    name="user",
+    description="The user whose profile you wish to see. If none, you'll see yours",
+    type=OptionType.USER,
+    required=False
+)
+
+# def get_random_quote() -> str:
+#     for _ in range(3):
+#         response = get(f"{BASE_URL}/quotes/random", verify=False)
+#         if response.status_code != 200:
+#             continue
+#         return response.json().pop()["content"]
+#     return "No description"
 
 def get_random_color() -> Color:
     red = randint(0, 255)
@@ -21,15 +29,16 @@ def get_random_color() -> Color:
 
 @slash_command(
     name="profile",
-    description="See your user profile with the bot"
+    description="See your user profile with the bot",
+    options=[USER_OPTION]
 )
-async def profile(ctx: SlashContext):
-    user_id = str(ctx.author.id)
-    bot_user = BotUser(user_id)
+async def profile(ctx: SlashContext, user: User | Member | None=None):
+    user = user or ctx.author
+    bot_user = BotUser(str(user.id))
 
-    name = bot_user.name or ctx.author.username
-    description = bot_user.description or get_random_quote()
-    image = EmbedAttachment(url=ctx.author.avatar_url)
+    name = bot_user.name or user.username
+    description = bot_user.description
+    image = EmbedAttachment(url=user.avatar_url)
 
     embed = Embed(
         title=f"{name}'s profile",
