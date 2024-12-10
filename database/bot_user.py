62527c2d74eee1d67560bd3abc48ec8ddf7b58fe
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2024-12-10 15:29:39 -0500

    REFACTOR: dried my update method

--- database/bot_user.py
+++ database/bot_user.py
@@ -1,5 +1,9 @@
-import asyncio
+from collections.abc import Mapping, Sequence
+from typing import Any
 
+from pymongo.results import UpdateResult
+
+from models.game_stat import GameStat
 from .mongo_database import BotDatabase
 from datetime import datetime, timedelta
 
@@ -23,6 +27,12 @@ class BotUser:
     def __find_user(self):
         return self.__collection.find_one({"_id": self.user_id})
 
+    def __update(self, update: Mapping[str, Any] | Sequence[Mapping[str, Any]]) -> UpdateResult:
+        return self.__collection.update_one(
+            {"_id": self.user_id},
+            update
+        )
+
     @property
     def balance(self):
         return self.__find_user().get("balance")
@@ -36,27 +46,18 @@ class BotUser:
         return self.__find_user().get("name")
 
     def set_name(self, new_name: str) -> None:
-        self.__collection.update_one(
-            {"_id": self.user_id},
-            {"$set": {"name": new_name}}
-        )
+        self.__update({"$set": {"name": new_name}})
 
     @property
     def description(self) -> str | None:
         return self.__find_user().get("description")
 
     def set_description(self, new_description: str) -> None:
-        self.__collection.update_one(
-            {"_id": self.user_id},
-            {"$set": {"description": new_description}}
-        )
+        self.__update({"$set": {"description": new_description}})
 
 
     def __increment_balance(self, amount: int):
-        self.__collection.update_one(
-            {"_id": self.user_id},
-            {"$inc": {"balance": amount}}
-        )
+        self.__update({"$inc": {"balance": amount}})
 
     def deposit(self, amount: int):
         if amount < 0:
@@ -83,7 +84,7 @@ class BotUser:
 
     def claim_reward(self, reward_amount: int):
         self.deposit(reward_amount)
-        self.__collection.update_one(
-            {"_id": self.user_id},
-            {"$set": {"last_reward": datetime.now()}}
-        )
+        self.__update({"$set": {"last_reward": datetime.now()}})
+
+    def add_game_stat(self, game_stat: GameStat) -> None:
+        pass
