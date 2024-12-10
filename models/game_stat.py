a543fed4205f09b25ef3ac30340627192b1dd463
Kasra <97859344+KasraRahimi@users.noreply.github.com>
2024-12-10 15:19:01 -0500

    FEAT: implemented game stat data class

---
+++ models/game_stat.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass, asdict
+from datetime import datetime
+from typing import Self
+
+@dataclass
+class GameStat:
+    name: str
+    date: datetime
+    bet: int
+    payout: int
+    is_win: bool
+
+    @classmethod
+    def from_json(cls, json: dict) -> Self | None:
+        try:
+            return cls(
+                name=json['name'],
+                date=json['date'],
+                bet=json['bet'],
+                payout=json['payout'],
+                is_win=json['is_win']
+            )
+        except KeyError:
+            return None
+
+    def to_json(self) -> dict:
+        return asdict(self)
