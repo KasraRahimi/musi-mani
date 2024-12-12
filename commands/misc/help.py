b16e7aa034ffde9a7d40137e6c8f07a5ad741a4b
KasraRahimi <kasra.rahimi@polymtl.ca>
2024-12-12 14:57:47 -0500

    FEAT: Added an embed generator

--- commands/misc/help.py
+++ commands/misc/help.py
@@ -1,8 +1,21 @@
+from datetime import datetime
 from dataclasses import dataclass
-import commands
-from interactions import slash_command, Button, ButtonStyle, SlashContext, SlashCommand, ActionRow
+from enum import StrEnum
 
+from interactions.client.utils import timestamp_converter
+
+import commands
+from interactions import slash_command, Button, ButtonStyle, SlashContext, SlashCommand, ActionRow, EmbedField, \
+    ComponentContext, InteractionContext, Embed, EmbedAuthor
+
+from commands.user_profile.set_description import description_option
 from commands.utils import get_button_id
+
+class Page(StrEnum):
+    MISC_MODULE = 'misc'
+    TALAN_MODULE = 'talan'
+    PROFILE_MODULE = 'user_profile'
+    GAMES_MODULE = 'games'
 
 DOCS_BUTTON = ActionRow(Button(
     style=ButtonStyle.LINK,
@@ -10,38 +23,73 @@ DOCS_BUTTON = ActionRow(Button(
     label="Documentation",
 ))
 
-MISC_MODULE = 'misc'
-TALAN_MODULE = 'talan'
-PROFILE_MODULE = 'user_profile'
-GAMES_MODULE = 'games'
-
 def get_page_changing_action_row(ctx: SlashContext) -> ActionRow:
     return ActionRow(
         Button(
             style=ButtonStyle.SECONDARY,
             label="Misc",
-            custom_id=get_button_id(MISC_MODULE, ctx),
+            custom_id=get_button_id(Page.MISC_MODULE, ctx),
             emoji="🔮"
         ),
         Button(
             style=ButtonStyle.SECONDARY,
             label="Talan",
-            custom_id=get_button_id(TALAN_MODULE, ctx),
+            custom_id=get_button_id(Page.TALAN_MODULE, ctx),
             emoji="🪙",
         ),
         Button(
             style=ButtonStyle.SECONDARY,
             label="Profile",
-            custom_id=get_button_id(PROFILE_MODULE, ctx),
+            custom_id=get_button_id(Page.PROFILE_MODULE, ctx),
             emoji="ℹ️"
         ),
         Button(
             style=ButtonStyle.SECONDARY,
             label="Games",
-            custom_id=get_button_id(GAMES_MODULE, ctx),
+            custom_id=get_button_id(Page.GAMES_MODULE, ctx),
             emoji="🎲"
         )
     )
+
+def get_embed_by_page(ctx: InteractionContext, page: Page):
+    command_infos = get_command_infos_from_module(page)
+    embed_fields = []
+    for command_info in command_infos:
+        embed_fields.append(
+            EmbedField(
+                name=command_info.call_name,
+                value=command_info.description,
+                inline=True
+            )
+        )
+
+    match page:
+        case Page.TALAN_MODULE:
+            name = "Talan"
+            description = "The list of commands relating to the bot's economy and currency system"
+        case Page.GAMES_MODULE:
+            name = "Games"
+            description = "The list of commands relating to the various games a user can place bets on and play"
+        case Page.PROFILE_MODULE:
+            name = "Profile"
+            description = "The list of commands relating to a user's profile on the bot"
+        case Page.MISC_MODULE:
+            name = "Miscellaneous"
+            description = "The list of miscellaneous commands"
+
+    return get_embed(ctx, name, description, embed_fields)
+
+def get_embed(ctx: InteractionContext, name: str, description: str, embed_fields: list[EmbedField]) -> Embed:
+    author = EmbedAuthor(name=ctx.bot.user.username, icon_url=ctx.bot.user.avatar_url)
+    timestamp = timestamp_converter(datetime.now())
+    return Embed(
+        title=name,
+        description=description,
+        timestamp=timestamp,
+        fields=embed_fields,
+        author=author
+    )
+
 
 @dataclass
 class CommandInfo:
