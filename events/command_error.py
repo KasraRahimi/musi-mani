7140c1477cf1c39d37f53f1ad55a21d49b6a4fe7
KasraRahimi <kasra.rahimi@polymtl.ca>
2024-12-13 14:29:24 -0500

    FEAT: added logger for command errors

--- events/command_error.py
+++ events/command_error.py
@@ -1,27 +1,19 @@
-import traceback
 from datetime import datetime
-from os import makedirs
-
+import logging
 from interactions import Listener
 from interactions.api.events import CommandError
 from interactions.client.errors import CommandCheckFailure
 
-EVENT_NAME = "command_error"
+from constants import MAIN_LOGGER
 
-LOGS_DIRECTORY = "../logs/"
+EVENT_NAME = "command_error"
 
 async def callback(event: CommandError):
     if isinstance(event.error, CommandCheckFailure):
         await event.ctx.send("You do not have the permissions to use this command", ephemeral=True)
         return
-    now = datetime.now()
-    today = now.date().isoformat()
-    makedirs(LOGS_DIRECTORY, exist_ok=True)
-    with open(f"../logs/{today}-logs.txt", "a") as file:
-        time = now.strftime("%H:%M:%S")
-        file.write(f"=== {time} ===\n")
-        traceback.print_exception(event.error, file=file)
-        file.write("\n")
+    logger = logging.getLogger(MAIN_LOGGER)
+    logger.error("An error occurred while executing a command:", exc_info=True, stack_info=True)
     await event.ctx.send("Something went wrong", ephemeral=True)
 
 command_error = Listener(event=EVENT_NAME, func=callback, disable_default_listeners=True)
