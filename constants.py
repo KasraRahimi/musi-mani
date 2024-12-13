825f0ea668a39a42f8b4e06113f5c702b8ccf34e
KasraRahimi <kasra.rahimi@polymtl.ca>
2024-12-13 12:47:01 -0500

    FEAT: added main logger name and root directory

--- constants.py
+++ constants.py
@@ -1,7 +1,12 @@
 from dotenv import load_dotenv
 from os import getenv
+from pathlib import Path
 
 load_dotenv()
 
 TOKEN = getenv("TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI")
+
+ROOT_DIRECTORY = Path(__file__).parent.resolve()
+
+MAIN_LOGGER = "bot_logger"
