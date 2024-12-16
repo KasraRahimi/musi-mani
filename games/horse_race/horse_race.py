f50ec62ed36e7937ecb41f11d52f21843950696f
KasraRahimi <kasra.rahimi@polymtl.ca>
2024-12-16 17:23:27 -0500

    FIX: Added method to calculate probabilites that horses win

--- games/horse_race/horse_race.py
+++ games/horse_race/horse_race.py
@@ -1,6 +1,9 @@
+from functools import cached_property
+
 from horse import Horse
 from random import random
 from math import comb
+from itertools import product
 
 def generate_horse_step_probabilities(horses: list[Horse], num_of_trials: int=2) -> None:
     for horse in horses:
@@ -21,8 +24,8 @@ def normalize_horse_step_probabilities(horses: list[Horse]) -> None:
 
 def calculate_probability_of_win_with_steps(
         index_of_winner: int,
-        num_of_loser_steps: tuple[int],
-        step_probabilities: tuple[float],
+        num_of_loser_steps: tuple[int, int, int],
+        step_probabilities: tuple[float, ...],
         steps_to_victory: int=7
 ) -> float:
     num_of_steps = (
@@ -44,17 +47,38 @@ def calculate_probability_of_win_with_steps(
 
 
 class HorseRace:
-    def __init__(self, bet: int, num_of_steps_to_victory: int=7):
+    def __init__(self, bet: int, num_of_steps_to_victory: int=7, step_probabilities: tuple[float, ...]=None):
         self.num_of_steps_to_victory = num_of_steps_to_victory
         self.bet = bet
         self.horses = [Horse() for _ in range(4)]
-        generate_horse_step_probabilities(self.horses)
-        normalize_horse_step_probabilities(self.horses)
+        if step_probabilities is None:
+            generate_horse_step_probabilities(self.horses)
+            normalize_horse_step_probabilities(self.horses)
+        else:
+            for horse, probablity in zip(self.horses, step_probabilities):
+                horse.step_probability = probablity
+
+    @cached_property
+    def calculate_win_probability(self) -> tuple[float, ...]:
+        win_probabilities = [0 for _ in range(4)]
+        step_probabilities = tuple([horse.step_probability for horse in self.horses])
+        for i, j, k in product(range(self.num_of_steps_to_victory), repeat=3):
+            for index, win in enumerate(win_probabilities):
+                win_probabilities[index] += calculate_probability_of_win_with_steps(
+                    index_of_winner=index,
+                    num_of_loser_steps=(i, j, k),
+                    step_probabilities=step_probabilities,
+                    steps_to_victory=self.num_of_steps_to_victory,
+                )
+        return tuple(win_probabilities)
+
 
 if __name__ == '__main__':
-    horse_race = HorseRace(0, num_of_steps_to_victory=7)
-    sum_horse_probabilities = 0
+    horse_race = HorseRace(bet=0, num_of_steps_to_victory=7)
     for horse in horse_race.horses:
         print(horse)
-        sum_horse_probabilities += horse.step_probability
-    print(f'{sum_horse_probabilities=}')
+    print(horse_race.calculate_win_probability)
+    print(horse_race.calculate_win_probability)
+    print(horse_race.calculate_win_probability)
+    print(horse_race.calculate_win_probability)
+    print(sum(horse_race.calculate_win_probability))
