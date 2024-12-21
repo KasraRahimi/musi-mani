6f710ad9a2f9f6e5152a2aa47837f3a651c5d88c
KasraRahimi <kasra.rahimi@polymtl.ca>
2024-12-21 15:44:01 -0500

    FEAT: added  method for clearing tables

--- database/sql_db.py
+++ database/sql_db.py
@@ -32,3 +32,55 @@ class SQLDb:
     def get_connection(self) -> MySQLConnectionAbstract | PooledMySQLConnection:
         return self.__pool.get_connection()
 
+    def drop_users_table(self) -> None:
+        with self.__pool.get_connection() as cnx:
+            cursor = cnx.cursor()
+            cursor.execute("DROP TABLE IF EXISTS users")
+
+    def drop_game_stats_table(self) -> None:
+        with self.__pool.get_connection() as cnx:
+            cursor = cnx.cursor()
+            cursor.execute("DROP TABLE IF EXISTS game_stats")
+
+    def create_users_table(self) -> None:
+        with self.__pool.get_connection() as cnx:
+            cursor = cnx.cursor()
+            cursor.execute(
+                """
+                CREATE TABLE users (
+                    id          BIGINT UNSIGNED PRIMARY KEY,
+                    name        VARCHAR(32),
+                    description VARCHAR(128),
+                    balance     INTEGER NOT NULL,
+                    last_reward DATETIME
+                )
+                """
+            )
+
+    def create_posts_table(self) -> None:
+        with self.__pool.get_connection() as cnx:
+            cursor = cnx.cursor()
+            cursor.execute(
+                """
+                CREATE TABLE game_stats (
+                    id          INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+                    user_id     BIGINT UNSIGNED NOT NULL,
+                    name        VARCHAR(128) NOT NULL,
+                    date        DATETIME NOT NULL,
+                    bet         INTEGER NOT NULL,
+                    payout      INTEGER NOT NULL,
+                    is_win      BOOLEAN NOT NULL,
+                    FOREIGN KEY (user_id) REFERENCES users(id)
+                        ON DELETE CASCADE
+                )
+                """
+            )
+
+    def reset_all_data(self) -> int:
+        with self.__pool.get_connection() as cnx:
+            cursor = cnx.cursor()
+            cursor.execute("SELECT COUNT(*) FROM users")
+            count = cursor.fetchone()
+            cursor.execute("DELETE FROM users")
+            cursor.execute("DELETE FROM game_stats")
+        return count[0]
